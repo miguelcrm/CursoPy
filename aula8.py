a94cbded80b5f5fb2b95fc2678c80fc060b1539f
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 18:31:41 -0300

    Update aula8.py

--- aula8.py
+++ aula8.py
@@ -26,3 +26,21 @@ se ele conter quebrados ou não '''
 
 divisao_inteira = 100 // 5.2
 print(divisao_inteira)
+
+# Exponenciação 
+
+exponenciacao = 2 ** 10
+print(exponenciacao)
+
+''' Será o mesmo que 2^10 ou 2 elevado a 10'''
+
+# Modulo ou resto da divisão
+
+modulo = 55 % 2
+print(modulo)
+
+''' Se fizermos 25 % 5, não terá o resto por conta da divisão
+conter um resultado inteiro '''
+
+modulo2 = 25 % 5
+print(modulo2)
