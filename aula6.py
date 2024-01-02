b4a5f827606fdd4ac885a33db72abd9b02fe0215
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 17:21:48 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -8,18 +8,15 @@ print(type('1'))
 print(1)
 print(type(1))
 
-''' Aqui temos o tipo int e dentro dela temos o número 1 '''
+''' Aqui temos o tipo int e dentro dela também temos o número 1 '''
 
-''' Se por acaso você estiver em um programa ou crie
-uma calculadora por exemplo e a informação que seu código
-recebe seja um str mas você tem que somar essa str
-com outro valor, podemos usar a conversão do valor'''
+''' E aqui utilizamos a coersão de tipos usando qualquer tipo
+de dado.'''
 
-print(int('1'), type(int('1')))
 print(int('1') + 1)
 print(float('1.5') + 1)
 print(str(15) + ' ' + 'meia 15')
-print(bool(  ))
+
 
 # Se tentarmos colocar
 
