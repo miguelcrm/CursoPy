3d65e94d81d0eab0ae816a68f6222dba34db4f3b
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 18:21:43 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,28 @@
+# Operadores aritméticos 
+
+# Adição
+
+adicao = 10 + 10
+print(adicao)
+
+# Subtração 
+
+subtracao = 10 - 8
+print(subtracao)
+
+# Multiplicação 
+
+multiplicacao = 10 * 2
+print(multiplicacao)
+
+#Divisão 
+
+divisao = 100 / 5.2
+print(divisao)
+
+''' A única diferença entre as divisões e que a divisão
+inteira sempre retornará um número inteiro independente 
+se ele conter quebrados ou não '''
+
+divisao_inteira = 100 // 5.2
+print(divisao_inteira)
