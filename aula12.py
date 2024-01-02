b9f9efc4085b0bcdde35bcda700e4385bec65612
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 19:30:33 -0300

    Create aula12.py

---
+++ aula12.py
@@ -0,0 +1,16 @@
+# Cálculo do IMC (Indíce de Massa Corporal)
+
+''' Elipses = ..., funciona como algo que ainda será escrito '''
+
+nome = 'Miguel Camargo'
+altura = 1.82
+peso = 68
+
+# IMC = peso / (altura x altura)
+
+imc = peso / (altura ** 2)
+
+print('Nome:', nome)
+print('Altura:', altura)
+print('Pesa:', peso)
+print('E o seu IMC é:', imc)
