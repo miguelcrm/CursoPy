2ad586a826f32317826d4c24e6cab58dc68b0841
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 21:05:58 -0300

    Create DocString.py

---
+++ DocString.py
@@ -0,0 +1,10 @@
+# - Criação de comentários que só afetará essa única linha
+
+"""
+DocString- Utilizado para escrever um comentário em quantas
+linhas você quiser.
+"""
+''' DocString '''
+
+# Python é uma linguagem dinâmica e forte. 
+# Tudo em Python é um objeto. 
