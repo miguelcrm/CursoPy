bcd68127c91fa5c56263f9985481025837a8c356
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 21:27:15 -0300

    Create sep_end.py

---
+++ sep_end.py
@@ -0,0 +1,45 @@
+''' Aula 2 '''
+
+# Elemento de saída = Print
+
+print('Hello, World!')
+print(12, 34, sep='.')
+print(56, 78, sep ="-")
+
+''' Sep = Separador
+    Pode ser usado para substituição de qualquer símbolo
+    a sua escolha nos espaços da linha de saída do código
+'''
+
+# Elemento de saída = Print
+
+print(21, 43, sep='.', end='$\r\n')
+print(46, 98, sep ="-", end='%\r\n')
+print(66, 78, sep ="*", end=' = 5148\r\n')
+
+'''
+End = Final 
+Dita um simbolo ou o que quiser colocar no final da linha de
+código. 
+'''
+
+''' Aula 3 '''
+
+# Tipo de dados str ou string ("") ('')
+
+#String são textos entre as aspas.
+
+print("Miguel Camargo Raposo de Melo")
+
+# Os escapes é usado quando se precisa colocar algum carácter.
+# E esse carácter for inválido na inserção como string.
+# Como as "" ''.
+
+''' Escapes = \ '''
+
+print("\'Miguel Camargo Raposo de Melo\'")
+
+# Ou podemos inverter as aspas e utilizar o print da seguinte forma:
+
+print('"Miguel Camargo Raposo de Melo"')
+
