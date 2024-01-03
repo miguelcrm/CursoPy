ce886197e550925386df5dfd2653ded6e147d95b
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-02 21:51:41 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,49 @@
+''' Variáveis são usadas para armazenar algo na memória do 
+computador.'''
+
+# E depois ela retorna atráves da saída.
+
+# Nome da pessoa.
+
+nome_completo = 'Miguel Camargo Raposo de Melo'
+
+''' Quando separamos palavras com ''_'' chamamos isso de 
+snack case '''
+
+print('Nome:', nome_completo)   # Elementos de saída e os valores
+                                # Armazenados
+
+#
+
+# Idade da pessoa 
+
+idade = 21 
+maior_de_idade = idade >= 18
+
+print('É maior de idade?', maior_de_idade)
+print('Idade:', idade, 'anos')
+
+altura = 1.82
+print('Altura:', str(altura) + 'm')
+#
+trabalha_com = 'Tecnologia'
+print('Trabalha com:', trabalha_com)
+
+# Sálario inicial e final 
+
+quantidade_salario = 2800
+despesas = 600
+salario = quantidade_salario
+final = quantidade_salario - despesas
+print('Recebe:', salario)
+print('Despesas:', despesas)
+print( 'Salário Bruto Final:', final)
+
+#
+
+
+''' Aqui está um exemplo de armazenamento de informações 
+    em uma variável e seu retorno, de modo simples e fácil
+    podemos brincar com isso alternando o código e ele te
+    trará o resultado, com o nome da pessoa, o salário, suas
+    despesas e seu salário bruto '''
