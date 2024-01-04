d9718a2c0c902f43a7cfde4f6e212c4bf079d3f1
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-04 19:26:36 -0300

    Update formatacao_format.py

--- formatacao_format.py
+++ formatacao_format.py
@@ -5,18 +5,18 @@ c = 2.55
 ''' Podemos colocar as variáveis com o metódo format para serem
 utilizadas como outra forma de saída '''
 
-string = 'a = {nome1}, b = {nome2}, c = {nome3:.3f}'
+string = 'a = {}, b = {nome2}, c = {nome3:.3f}'
 
 # Saída 
 formato = string.format(
-    nome1 = a,
-    nome2 = b,
+    a, nome2 = b,
     nome3 = c)
 print(formato)
 
 ''' 'NOME' = Será um parâmetro nomeado.
     Parâmetro nomeado será um objeto.
-    Será usado em ordem.
+    Será usado em ordem e tudo que vier depois da nomeação
+    também tem que ser nomeado.
     E precisam também está em ''string'' que é a primeira
     variável. 
 '''
