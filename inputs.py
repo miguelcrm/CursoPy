be6cafd433708147eb49358ff4e6b9b47e39dae2
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-04 19:34:34 -0300

    Create inputs.py

---
+++ inputs.py
@@ -0,0 +1,13 @@
+# input = será uma forma para coletar dados do usuário.
+''' O programa não será finalizado no terminal até que o usuário
+    faça a interação com a input criada.
+'''
+
+# input('Qual é o seu nome? ')
+
+# E podemos atribuir uma variável para essa input.
+# ''nome'' será a variável. 
+
+nome = input('Qual é o seu nome? ')
+
+print(nome)
