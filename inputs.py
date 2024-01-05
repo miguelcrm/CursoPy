b47f08fc06c3c38947b4dcfeb85f7c80545785d0
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-04 22:00:56 -0300

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -10,4 +10,20 @@
 
 nome = input('Qual é o seu nome? ')
 
-print(nome)
+print(f'O seu nome é: {nome} \r\n')
+
+
+# Outro exemplo usando números. 
+''' Como as variáveis sempre serão uma str temos que fazer
+    uma conversão de tipos para que não gere erro.
+'''
+
+numero1 = input(f'{nome} digite um número: ')
+numero2 = input('Digite outro número: ')
+
+# Fazendo a conversão.
+
+intnumero1 = int(numero1)
+intnumero2 = int(numero2)
+
+print(f'A soma dos números são: {intnumero1 + intnumero2}')
