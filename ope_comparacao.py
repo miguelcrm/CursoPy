90a800c0fc2eec16107357870766b115183e6fe8
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-06 18:50:25 -0300

    Create ope_comparacao.py

---
+++ ope_comparacao.py
@@ -0,0 +1,33 @@
+'''
+OPERADORES DE COMPARAÇÃO (RELACIONAIS)
+
+>   maior
+>=  maior ou igual
+<   menor
+<=  menor ou igual
+==  igual
+!=  diferente 
+
+'''
+
+maior = 2 > 1 #True
+print(maior)
+
+maior_ou_igual = 2 >= 1 #True
+print(maior_ou_igual)
+
+menor = 1 < 2 #True
+print(menor)
+
+menor_ou_igual = 2 <= 2 #True
+print(menor_ou_igual)
+
+igual = 'a' == 'a' #True
+print(igual)
+
+diferente = 'a' != 'b' #True
+print(diferente)
+
+''' Sempre que usar um operador de comparação ele irá retornar
+    true ou false a sua comparação.
+'''
