5486708fc5f0869d041b2cebf3a6c9ef37736282
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-06 18:20:11 -0300

    Create debugger.py

---
+++ debugger.py
@@ -0,0 +1,34 @@
+# Debugger = retirar um problema do seu código, depurando ele.
+# Usando o VS Code e o interpretador do Python.
+
+''' Ao lado da contagem de linha de código terá um ponto verme
+lho chamado ''Breakpoint''
+
+    Utilizando o breakpoint fora de uma linha de código, ele
+    não irá parar e irá rodar o código inteiro. Temos que usar 
+    o breakpoint como marca na primeira linha de código existen
+    te.
+
+    Após marca seu breakpoint e começar a depuração do seu códi
+    go, ele irá abrir um pequeno painel de opções. E utilizando
+    ''Step Over'' ou ''Contornar'' 
+'''
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para execução da condição 1')
+
+elif condicao2:
+    print('Código para execução da condição 2')
+
+elif condicao3:
+    print('Código para execução da condição 3')
+
+elif condicao4:
+    print('Código para execução da condição 4')
+
+else :
+    print('Nenhuma condição foi aceita!')
