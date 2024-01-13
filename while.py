18113002bf3b85e3f51a37921e948834daefbf1c
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-12 23:27:04 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,45 @@
+# While
+
+'''
+While = Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+
+Loop Infinito - Quando um código não tem fim.
+
+# Isso é um exemplo de código de Loop Infinito,
+tem que tomar cuidado, que isso pode gerar um transtorno
+em seu computador, por repetir ele várias vezes, caso não
+consiga parar.
+
+condicao = True
+
+while condicao:
+    print(1)
+
+'''
+
+# Mas se fizermos da seguinte maneira
+
+# Se a condicao for ''True'', e enquanto ela for ''True''
+# ela irá rodar o que está dentro do bloco de código while
+
+condicao = True
+
+while condicao:
+    nome = input('Digite seu nome: ')
+    
+    if nome == 'sair' or nome == 'Sair':
+        break
+
+    print(f'Seu nome é: {nome}') 
+    
+# Se usarmos o Debugger iremos ver como funciona o While.
+# Ele irá rodar trecho por trecho de código, e você irá perceber
+# Que enquanto a condicao for ''True'', ele volta ao While.
+
+# Podemos utilizar ''break'' para parar o laço While.
+    # Se o usuário escrever ''sair'' em nome, ele irá ativar o break.
+
+print('Acabou')    
+
