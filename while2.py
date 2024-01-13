9c425a0570ba62c43dc02642c20190dbf6f55f0e
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-12 23:27:06 -0300

    Create while2.py

---
+++ while2.py
@@ -0,0 +1,11 @@
+# Outro exemplo de código com While
+
+# Se caso quisermos que um algoritmo conte 0, 1, 2, 3...20
+
+contador = 0
+
+while contador < 21:
+    print(contador)
+    contador = contador + 1
+
+
