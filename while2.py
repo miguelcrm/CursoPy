fc5432de23905c3addb15777e6f9f3eafbfabce0
Miguel Raposo <miguel.melocr@gmail.com>
2024-01-13 22:40:18 -0300

    Update while2.py

--- while2.py
+++ while2.py
@@ -2,11 +2,30 @@
 
 # Se caso quisermos que um algoritmo conte 0, 1, 2, 3...20
 
+'''
 contador = 0
 
-while contador < 31:
-    print(contador)
+while contador < 30:
     contador = contador + 1
+    print(contador)
+'''
+
+# Operadores de atribuição + While
+
+'''
+Podemos fazer da seguinte maneira, usando os operadores de atribuição.
+
+'=' '+=' '-=' '*=' '/=' '//=' '**=' '%='
+
+'''
+
+contador = 0
+
+while contador < 30: # Enquanto o contador for menor que 30
+    contador += 1 # Adicione mais um
+    print(contador) # Imprima contador
+
+    
 
 
 
